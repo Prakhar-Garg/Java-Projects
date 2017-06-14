8232854ab18f9c4623311e992dc976092f02f0a4
Prakhar-Garg <prakhargarg.cse18@jecrc.ac.in>
2017-06-14 21:06:14 +0530

    this program explains the use of arithmetic operators

---
+++ Arithmetic.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+import java.math.*;
+
+public class Arithmetic {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        double mealCost = scan.nextDouble(); // original meal price
+        int tipPercent = scan.nextInt(); // tip percentage
+        int taxPercent = scan.nextInt(); // tax percentage
+        scan.close();
+      
+      double tip= mealCost*((double)tipPercent/100); // Write your calculation code here.
+      double tax= mealCost*((double)taxPercent/100);
+        // cast the result of the rounding operation to an int and save it as totalCost 
+        int totalCost = (int) Math.round(mealCost+tip+tax);
+      System.out.println("The total meal cost is"+" "+totalCost+" "+"dollars");
+        // Print your result
+    }
+}
