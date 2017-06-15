b257b475d72bdacaed8909fc635cdea3b0ec150f
Prakhar-Garg <prakhargarg.cse18@jecrc.ac.in>
2017-06-15 22:51:14 +0530

    Add files via upload

---
+++ RelationalOperator.java
@@ -0,0 +1,15 @@
+//A program on relational operator
+
+class RelationalOperator{
+	public static void main(String args[]){
+		System.out.println(5<4);
+		System.out.println(5>4);
+		System.out.println(5>=6);
+		System.out.println(5>=5);
+		//System.out.println(5>4>3); 
+		//This statement will give error since when we compare5 and 4 then there result will be 
+		// boolean value and then the result is compared with 3 i.e.int value so their type will
+		// be mismatch
+		
+	}
+}
