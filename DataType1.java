05a34e083e0636c745378aa44d913f9f044ca10e
Prakhar-Garg <prakhargarg.cse18@jecrc.ac.in>
2017-06-19 23:40:23 +0530

    Add files via upload

---
+++ DataType1.java
@@ -0,0 +1,23 @@
+//in this program we are concatnating input taken directly and input which are read
+
+import java.util.Scanner;
+class DataType1{
+	public static void main(String args[]){
+		int i=4;
+		double d=4.0;
+		String s="Hackerrank";
+		Scanner sc=new Scanner(System.in);
+	  int a= sc.nextInt();
+	  double b=sc.nextDouble();
+      String c = sc.next();
+      System.out.println(i+a);
+      System.out.println(d+b);
+      System.out.println(s+c);
+	}
+}      
+      
+      
+      
+      
+      
+      
