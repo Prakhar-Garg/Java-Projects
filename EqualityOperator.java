fc612a65a3dc3db3aefd93888a103ef7e7e55c13
Prakhar-Garg <prakhargarg.cse18@jecrc.ac.in>
2017-06-22 23:21:36 +0530

    Add files via upload

---
+++ EqualityOperator.java
@@ -0,0 +1,10 @@
+//A program on equality operator
+class Equality{
+	public static void main(String args[]){
+		System.out.println(5==4);		//false
+		System.out.println(5!=4);		//true
+		//System.out.println(5==5==5);	
+		/* error: incomparable types: boolean and int
+		 * System.out.println(5==5==5);*/
+	}
+}
